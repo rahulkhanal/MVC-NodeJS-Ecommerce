e549fb78383a6606167f6390aa79963bb8edaac5
Rahul Khanal <rahul39khanal@gmail.com>
2023-08-15 15:08:23 +0545

    static file and hbs setup

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
 //package imports
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 const express = require("express");
 var bodyParser = require("body-parser");
 const app = express();
-dotenv.config()
-const PORT = process.env.PORT || 9000;
-const URL = process.env.DATABASE_URL || "mongodb://127.0.0.1:27017";
+dotenv.config();
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "/view")));
+app.set("view engine", "hbs");
+app.set("views", "./view");
 
 //file imports
+const PORT = process.env.PORT || 9000;
+const URL = process.env.DATABASE_URL || "mongodb://127.0.0.1:27017";
 const middlewaresErr = require("./Error/middleware-error");
 const connectDB = require("./database/connection");
+const router = require("./Routes/route");
 
 //middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,7 +25,7 @@ app.use(bodyParser.json());
 
 //error middlewares
 app.use(middlewaresErr);
-
+app.use(router);
 //server
 const start = async () => {
   try {
