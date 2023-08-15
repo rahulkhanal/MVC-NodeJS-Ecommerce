7c163f1d7bd4200283c0cb49ee2fbb9821a1597c
Rahul Khanal <rahul39khanal@gmail.com>
2023-08-15 15:18:06 +0545

    ajax setup

--- Routes/route.js
+++ Routes/route.js
@@ -1,8 +1,11 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/", (req, resp) => {
+router.get("/login", (req, resp) => {
   resp.render("login.hbs");
+});
+router.get("/register", (req, resp) => {
+  resp.render("register.hbs");
 });
 
 module.exports = router;
